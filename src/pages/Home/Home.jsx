6b2e6b02113a4e4712bd4af6dfc2a890565fc4fa
kwon hana <kwon hana>
2025-11-04 14:50:22 +0900

    style: 라인베너, new-item  기능 및 스타일 추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import LineBanner from '../../component/layout/LineBanner';
+import NewItemMain from '../../component/collection/NewItemMain';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+import '../../styles/pages/home.scss';
 
 const Home = () => {
-  return <div>Home</div>;
+  const newItemTitle = [
+    { item: '보 플래드 백', no: '5BB173_2DU5_F0Z0F_V_OWM' },
+    { item: '트릭 가죽 키링', no: '5TL516_2E6Y_F0032' },
+    { item: '시어링 트래퍼 햇', no: '5HC386_2CSR_F0040' },
+    { item: '시어링 패치워크 스톨', no: 'MPY055_181P_F0038_S_OOO' },
+  ];
+
+  return (
+    <main className="main">
+      <div className="inner">
+        <section className="newItem">
+          <div className="fw-collection">
+            <Swiper
+              slidesPerView={1}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Autoplay, Pagination]}
+              className="mySwiper">
+              {newItemTitle.map((product, index) => (
+                <SwiperSlide key={index}>
+                  <NewItemMain item={product.item} no={product.no} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <div className="column-slide"></div>
+        </section>
+      </div>
+      <LineBanner />
+    </main>
+  );
 };
 
 export default Home;
