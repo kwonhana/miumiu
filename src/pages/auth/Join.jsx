0c7c754035032bad526bea1df6fe40d3fde3aa14
yujin42 <4yujin2@gmail.com>
2025-11-06 16:45:45 +0900

    머지

--- src/pages/auth/Join.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Join = () => {
-  return <div>회원가입</div>;
-};
-
-export default Join;
--- src/pages/auth/ResetID.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ResetID = () => {
-  return <div>아이디 찾기</div>;
-};
-
-export default ResetID;
