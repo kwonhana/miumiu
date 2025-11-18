2c645e6a97d6def350efdc7e873bf059251c80f8
yujin42 <4yujin2@gmail.com>
2025-11-18 11:21:04 +0900

    product상품 filter 오류 수정

--- src/pages/Products/layout/ProductList.jsx
+++ src/pages/Products/layout/ProductList.jsx
@@ -5,16 +5,22 @@ import { useProductsStore } from '../../../store/useProductsStore';
 const ProductList = () => {
   const { category1, category2, tags } = useParams();
 
-  const { filtered, onFecthItems, onCategorys, onCateOnly, onCateTag } = useProductsStore();
+  const { filtered, onFecthItems, onCateOnly, onCateTag, onCate1 } = useProductsStore();
 
   useEffect(() => {
     onFecthItems(); // 상품 불러오기
   }, []);
 
   useEffect(() => {
-    onCategorys(category1, category2, tags); // URL 변경 → 필터링 실행
-    onCateOnly(category1, category2);
-    onCateTag(category2, tags);
+    if (category1 && category2 && tags) {
+      onCateOnly(category1, category2);
+    } else if (category1 && tags && !category2) {
+      onCateTag(category1, tags);
+    } else if (category1 && category2 && !tags) {
+      onCateOnly(category1, category2);
+    } else if (category1 && !category2 && !tags) {
+      onCate1(category1);
+    }
   }, [category1, category2, tags]);
   console.log('상품이미지?', filtered);
 
@@ -23,7 +29,7 @@ const ProductList = () => {
       {filtered.map((p) => (
         <li className="item" key={p.id}>
           <Link to={`/product/${p.id}`}>
-            <img src={`/assets/images/detail/${p.local_detail_images[0]}`} alt={p.name} />
+            <img src={`/assets/images/detail/${p.local_detail_images?.[0]}`} alt={p.name} />
             <div className="product-text-box">
               <h3>{p.name}</h3>
               <p>{p.price}</p>
