d884155d6ddec5156df25904016ccf6d962dfbdb
kwon hana <kwon hana>
2025-11-18 15:28:19 +0900

    init: cart

---
+++ src/pages/Checkout/layout/CartItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../scss/CartItem.scss';
+import Button from '../../../component/layout/Button';
+
+const CartItem = () => {
+  return (
+    <div className="CartItem">
+      <div className="itemleft-Img">
+        <img src="" alt="" />
+      </div>
+      <div className="itemRight">
+        <div className="top">
+          <span className="itemNO">nonononononono</span>
+          <button className="icon"></button>
+        </div>
+        <p className="title">
+          titletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitletitle
+        </p>
+        <div className="bottom">
+          <div className="countBtn">
+            <button>-</button>
+            <span>1</span>
+            <button>+</button>
+          </div>
+          <p className="price">48593485930</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
