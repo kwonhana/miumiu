6344054a9e91509853fca33c74fae11ff9ed7d4f
kwon hana <kwon hana>
2025-11-25 16:12:19 +0900

    add: modal

---
+++ src/component/feedback/info/CookieSettings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CookieSettings = () => {
+  return (
+    <div>
+      CookieSettings
+      <h1>쿠키 설정</h1>
+    </div>
+  );
+};
+
+export default CookieSettings;
