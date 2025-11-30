6a45c6d281e54faa9789e1ea21c01394e8d415a8
jooyoung-s <jujuyy1225@gmail.com>
2025-11-30 14:21:57 +0900

    style:주말검수작업

---
+++ src/component/input/PasswordCheck.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './scss/PasswordCheck.scss';
+
+const PasswordCheck = () => {
+  return (
+    <div className="base-input password">
+      <div className="passCheck-wrap">
+        <div className="input-box">
+          <input type="password" placeholder="비밀번호를 입력해주세요" required />
+        </div>
+        <div className="icon"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordCheck;
